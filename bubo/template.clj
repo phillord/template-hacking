2ab97db859d206461d4bfd859c24393341437bd0
Phillip Lord <phillip.lord@russet.org.uk>
2025-05-27 16:24:27 +0100

    Add tawny recipe

---
+++ bubo/template.clj
@@ -0,0 +1,15 @@
+(cc/use 'tawny.repl)
+
+(def in (load-ontology (iri (clojure.java.io/file "in.owl"))))
+
+(cc/defn template [id label type]
+  (individual in id :type (class in type) :label label))
+
+(template
+ "ex:ind-1" "Individual 1" "Class_1")
+
+(.saveOntology
+ (owl-ontology-manager)
+ in
+ (org.semanticweb.owlapi.formats.ManchesterSyntaxDocumentFormat.)
+ (java.io.FileOutputStream. (java.io.File. "out.owl")))
